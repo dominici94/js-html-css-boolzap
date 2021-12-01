b2db29d769a71453a7c8fc1cbeb458f401e04fa2
dominici94 <mattiadominici94@gmail.com>
2021-12-01 14:35:48 +0100

    milestone 3

--- js/script.js
+++ js/script.js
@@ -6,9 +6,7 @@
 // ●   Visualizzazione dinamica della lista contatti:  tramite  la direttiva v-for, visualizzare 
 // nome e immagine di ogni contatto 
  
-// Milestone 3 
-// ●   Aggiunta di un messaggio  : l’utente scrive un testo  nella parte bassa e digitando 
-// “enter” il testo viene aggiunto al thread sopra, come messaggio verde 
+
 // ●   Risposta dall’interlocutore:  ad ogni inserimento di  un messaggio, l’utente riceverà 
 // un “ok” come risposta, che apparirà dopo 1 secondo. 
 // Milestone 4 
@@ -68,7 +66,7 @@ const app = new Vue({
                 {
                     date: '20/03/2020 16:35:00',
                     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'received'
+                    status: 'sent'
                 }
                 ],
             },
@@ -111,13 +109,23 @@ const app = new Vue({
             },
         ],
         activeItem : 0,
+        inputUser : '',
     },
     methods: {
         showContact: function(index){
-            this.activeItem = index;
-
+            this.activeItem = index;  
+        },
+        sendMessage: function(){
+            if(this.inputUser != ''){
+                this.contacts[this.activeItem].messages.push({
+                    date: '',
+                    message: this.inputUser,
+                    status: 'sent',
+                });
+                this.inputUser = '';
+            }
             
-        },  
+        },
     }
 }); 
 
@@ -126,3 +134,6 @@ const app = new Vue({
 // ●   Visualizzazione dinamica dei messaggi:  tramite la  direttiva v-for, visualizzare tutti i 
 // messaggi relativi al contatto attivo all’interno del pannello della conversazione 
 // ●   Click sul contatto  mostra la conversazione del contatto  cliccato
+// Milestone 3 
+// ●   Aggiunta di un messaggio  : l’utente scrive un testo  nella parte bassa e digitando 
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde 
