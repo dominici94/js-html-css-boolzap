da3a34257f2ef8c506be88803a75b54084c4a59c
dominici94 <mattiadominici94@gmail.com>
2021-12-03 10:03:34 +0100

    aggiustamenti codice

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-// Boolzapp - a (not very) innovative messaging 
-// platform 
-// Milestone 1 
-// ●   Replica della grafica  con la possibilità di avere  messaggi scritti dall’utente (verdi) e 
-// dall’interlocutore (bianco) assegnando due classi CSS diverse 
-// ●   Visualizzazione dinamica della lista contatti:  tramite  la direttiva v-for, visualizzare 
-// nome e immagine di ogni contatto 
- 
-// Milestone 2 
-// ●   Visualizzazione dinamica dei messaggi:  tramite la  direttiva v-for, visualizzare tutti i 
-// messaggi relativi al contatto attivo all’interno del pannello della conversazione 
-// ●   Click sul contatto  mostra la conversazione del contatto  cliccato
-// Milestone 3 
-// ●   Aggiunta di un messaggio  : l’utente scrive un testo  nella parte bassa e digitando 
-// “enter” il testo viene aggiunto al thread sopra, come messaggio verde 
-// ●   Risposta dall’interlocutore:  ad ogni inserimento di  un messaggio, l’utente riceverà 
-// un “ok” come risposta, che apparirà dopo 1 secondo.
-
- 
-
-
-
-
-
-
 const app = new Vue({
     el: '#root',
     data: {
@@ -136,28 +111,9 @@ const app = new Vue({
             }
         },
         findContact: function(){
-            for(let i=0; i< this.contacts.length; i++){
-                if(this.contacts[i].name.toLowerCase().includes(this.searchContact.toLowerCase())){
-                    this.contacts[i].visible = true;
-                }else{
-                    this.contacts[i].visible = false;
-                }
+            for(let i=0; i<this.contacts.length; i++){
+                this.contacts[i].visible = this.contacts[i].name.toLowerCase().includes(this.searchContact.toLowerCase());
             }
-            
         },
-              
-
     }
 }); 
-
-// Milestone 4 
-// ●   Ricerca utenti:  scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i 
-// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo 
-// “mar” rimangono solo Marco e Martina) 
-
-
-// stackoverflow per date
-// console.log(dayjs().format('DD/MM/YYYY hh:mm:ss')
-// );
-
-// milestone 4 ricerca: visible true,  quando stampate la lista dei contatti fate un controllo su qeusta variabile, quando effettui una ricerca alla pressione di un qualsiasi tasto su un campo input fate un ciclo su tutti i contatti e se la stringa è presente nel campo input rimane visible true altrimenti diventa visible false. come fate a verificare se all'interno di una stringa è oresente un'ltra stringa? si usa includes() perchè le stringhe sono come Array.
